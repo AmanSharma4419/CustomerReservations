9f90f86fbd48c14a75b5c54f9e21bc1bc448eba2
Aman <Sharmaaman4419@gmail.com>
2022-06-10 16:58:20 +0530

    cosmetic changes

--- src/App.tsx
+++ src/App.tsx
@@ -56,6 +56,9 @@ function App() {
           </div>
         </div>
         <div className="customer-food-container">
+          <div>
+            <h5 className="reservation-header">Customers</h5>
+          </div>
           {customerList.map((customer) => {
             return (
               <>
--- src/components/customerCard.tsx
+++ src/components/customerCard.tsx
@@ -5,13 +5,6 @@ const CustomerCard = ({ customer }: Props) => {
   return (
     <div className="customer-food-card-container">
       <p>{customer}</p>
-      <div className="customer-foods-container">
-        <div className="customer-food"></div>
-        <div className="customer-food-input-container">
-          <input />
-          <button>Add</button>
-        </div>
-      </div>
     </div>
   );
 };
